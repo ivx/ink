a166e17a9e0191dafee688e748b81c027647ce36
Mario Mainz <mario.mainz@invision.de>
2017-04-07 11:39:52 +0200

    Skip reports in Ink.FilterTranslator

--- test/ink/filter_translator_test.exs
+++ test/ink/filter_translator_test.exs
@@ -2,7 +2,7 @@ defmodule Ink.FilterTranslatorTest do
   use ExUnit.Case, async: true
 
   test "it falls back to Logger.Translator" do
-    assert :none == Ink.FilterTranslator.translate(
+    assert :skip == Ink.FilterTranslator.translate(
       :debug, :info, :report, {:progress, ["this", ["is", "SECRET"]]})
   end
 
@@ -10,7 +10,7 @@ defmodule Ink.FilterTranslatorTest do
     message = {'** Generic server MOEP', ["MOEP", {:msg, :moep}, %{}, :kapott]}
     expected_result = "GenServer \"MOEP\" terminating\n** (stop) :kapott\n" <>
       "Last message: {:msg, :[FILTERED]}\nState: %{}"
-    assert {:ok, expected_result} == Ink.FilterTranslator.translate(
+    assert :skip == Ink.FilterTranslator.translate(
       :debug, :error, :format, message)
   end
 end
