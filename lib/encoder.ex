378d3742b62fa5a8bdefbc78686ff63562bff5f1
Mario Mainz <mario.mainz@invision.de>
2017-04-13 11:45:00 +0200

    Exclude encoders from documentation

--- lib/encoder.ex
+++ lib/encoder.ex
@@ -1,28 +1,38 @@
 defimpl Poison.Encoder, for: PID do
+  @moduledoc false
+
   def encode(pid, options) do
     Poison.Encoder.BitString.encode(inspect(pid), options)
   end
 end
 
 defimpl Poison.Encoder, for: Port do
+  @moduledoc false
+
   def encode(port, options) do
     Poison.Encoder.BitString.encode(inspect(port), options)
   end
 end
 
 defimpl Poison.Encoder, for: Reference do
+  @moduledoc false
+
   def encode(ref, options) do
     Poison.Encoder.BitString.encode(inspect(ref), options)
   end
 end
 
 defimpl Poison.Encoder, for: Tuple do
+  @moduledoc false
+
   def encode(tuple, options) do
     Poison.Encoder.BitString.encode(inspect(tuple), options)
   end
 end
 
 defimpl Poison.Encoder, for: Function do
+  @moduledoc false
+
   def encode(function, options) do
     Poison.Encoder.BitString.encode(inspect(function), options)
   end
