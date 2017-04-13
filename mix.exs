cac49aed12c5a9c4d81f23482574cec5fe7e54db
Mario Mainz <mario.mainz@invision.de>
2017-04-13 11:49:13 +0200

    Bump version to 0.7.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Ink.Mixfile do
 
   def project do
     [app: :ink,
-     version: "0.7.1",
+     version: "0.7.2",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
