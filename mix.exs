1786df7367c9514181bb7be34814d328fdaf7ee4
Alexander Marold <alexander.marold@invision.de>
2017-06-07 13:29:53 +0200

    Updated dependencies

--- mix.exs
+++ mix.exs
@@ -48,7 +48,7 @@ defmodule Ink.Mixfile do
   defp deps do
     [{:poison, "~> 3.1"},
      {:inch_ex, "~> 0.5", only: :docs},
-     {:credo, "~> 0.7.0", only: [:dev, :test]},
+     {:credo, "~> 0.7", only: [:dev, :test]},
      {:ex_doc, ">= 0.0.0", only: :dev}]
   end
 end
