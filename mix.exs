4613d3e3967d706c296e72083993e96698c62c49
Peter Laudel <peter.laudel@invision.de>
2018-09-25 10:47:10 +0200

    Update elixir dependencies to new versions and bump the ink version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Ink.Mixfile do
   def project do
     [
       app: :ink,
-      version: "0.8.1",
+      version: "0.8.2",
       elixir: "~> 1.4",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
