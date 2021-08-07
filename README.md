90beec2b8ffb9eeefa07001e97ff2c3b0cee8b39
Kian-Meng, Ang <kianmeng@cpan.org>
2021-08-07 18:40:36 +0800

    Misc doc changes
    Besides other documentation changes, this commit ensures the generated
    HTML doc for HexDocs.pm will become the source of truth for this Elixir
    library and leverage on latest features of ExDoc.

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Ink
 
 [![Build Status](https://travis-ci.org/ivx/ink.svg?branch=master)](https://travis-ci.org/ivx/ink)
-[![Inline docs](http://inch-ci.org/github/ivx/ink.svg)](http://inch-ci.org/github/ivx/ink)
+[![Module Version](https://img.shields.io/hexpm/v/ink.svg)](https://hex.pm/packages/ink)
+[![Hex Docs](https://img.shields.io/badge/hex-docs-lightgreen.svg)](https://hexdocs.pm/ink/)
+[![Total Download](https://img.shields.io/hexpm/dt/ink.svg)](https://hex.pm/packages/ink)
+[![License](https://img.shields.io/hexpm/l/ink.svg)](https://github.com/ivx/ink/blob/master/LICENSE)
+[![Last Updated](https://img.shields.io/github/last-commit/ivx/ink.svg)](https://github.com/ivx/ink/commits/master)
 
 Ink is a backend for the Elixir `Logger` with two main purposes:
 
@@ -14,7 +18,9 @@ Just add `:ink` to your dependencies and run `mix deps.get`.
 
 ```elixir
 def deps do
-  [{:ink, "~> 1.0"}]
+  [
+    {:ink, "~> 1.0"}
+  ]
 end
 ```
 
@@ -41,3 +47,9 @@ at [the docs](https://hexdocs.pm/ink/Ink.html).
 - execute tests with `mix test`
 - update dependencies with `mix deps.update --all`
 - execute tests again `mix test`
+
+## Copyright and License
+
+Copyright (c) 2018 InVision AG
+
+This library is licensed under the [MIT License](./LICENSE.md).
--- mix.exs
+++ mix.exs
@@ -1,10 +1,14 @@
 defmodule Ink.Mixfile do
   use Mix.Project
 
+  @source_url "https://github.com/ivx/ink"
+  @version "1.2.0"
+
   def project do
     [
       app: :ink,
-      version: "1.2.0",
+      name: "Ink",
+      version: @version,
       elixir: "~> 1.8",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
@@ -12,55 +16,51 @@ defmodule Ink.Mixfile do
       package: package(),
       deps: deps(),
       aliases: aliases(),
-      name: "Ink",
-      source_url: "https://github.com/ivx/ink",
-      homepage_url: "https://github.com/ivx/ink",
-      docs: [
-        main: "Ink",
-        extras: ["README.md"]
-      ]
+      docs: docs()
     ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
     [extra_applications: [:logger]]
   end
 
   defp description do
     """
-    A backend for the Elixir Logger that logs JSON and can filter sensitive data.
     """
   end
 
   defp package do
     [
       name: :ink,
+      description:
+        "A backend for the Elixir Logger that logs JSON and can filter sensitive data.",
       files: ["lib", "mix.exs", "README*"],
       maintainers: ["Mario Mainz"],
       licenses: ["MIT"],
-      links: %{"GitHub" => "https://github.com/ivx/ink"}
+      links: %{"GitHub" => @source_url}
     ]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:jason, "~> 1.1"},
       {:inch_ex, "~> 2.0", only: :docs},
       {:credo, "~> 1.0", only: [:dev, :test]},
-      {:ex_doc, ">= 0.0.0", only: :dev}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp docs do
+    [
+      extras: [
+        "LICENSE.md": [title: "License"],
+        "README.md": [title: "Overview"]
+      ],
+      main: "readme",
+      source_url: @source_url,
+      source_ref: "v#{@version}",
+      homepage_url: @source_url,
+      formatters: ["html"]
     ]
   end
 
